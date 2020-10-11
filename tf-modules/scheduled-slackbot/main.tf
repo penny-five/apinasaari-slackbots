c9fbce48aa2806cc0295e11c5a3cd839817d0ebd
penny-five <lehton83@gmail.com>
2020-10-11 13:27:17 +0300

    automatically run terraform fmt

--- tf-modules/scheduled-slackbot/main.tf
+++ tf-modules/scheduled-slackbot/main.tf
@@ -76,7 +76,7 @@ resource "null_resource" "build" {
 
 resource "null_resource" "zip_files" {
   provisioner "local-exec" {
-    command = <<EOT
+    command     = <<EOT
       jq '.dependencies|=with_entries(select(.key|test("apinasaari-slackbots-.*")|not))' package.json > dist/package.json
       zip --junk-paths dist/app.zip dist/*
     EOT
